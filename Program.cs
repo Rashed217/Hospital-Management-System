6a7a0db9ff73e697d9e55420e400eb860601fffc
Rashed Al Ghailani <Authority217@gmail.com>
2024-09-29 14:57:40 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospital_MS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
