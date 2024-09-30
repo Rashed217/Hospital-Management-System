b36d5351784e914fc5ecb8c1621d6c0503fa6d1c
Rashed Al Ghailani <Authority217@gmail.com>
2024-09-30 10:03:11 +0400

    Added Room Type to (Room Class). and finished the main claas by adding Hospital, Doctors, Patients, Rooms.. etc.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace Hospital_MS
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Hospital hospital = new Hospital();
+
+            Doctor doctor1 = new Doctor("Alice", 35, Gender.Female, 1, "Cardiology");
+
+            Doctor doctor2 = new Doctor("Bob", 45, Gender.Male, 2, "Neurology");
+
+            hospital.AddDoctor(doctor1);
+            hospital.AddDoctor(doctor2);
+
+            Room room1 = new Room(101, RoomType.General);
+            Room room2 = new Room(102, RoomType.ICU);
+            hospital.RoomsList.Add(room1);
+            hospital.RoomsList.Add(room2);
+
+            Patient Patient1 = new Patient("John Doe", 30, Gender.Male, 1, "Heart Disease", doctor1);
+            Patient Patient2 = new Patient("Jane Smith", 25, Gender.Female, 2, "Migraine", doctor2);
+
+            hospital.AddPatient(Patient1);
+            hospital.AddPatient(Patient2);
+
+            hospital.AssignRoomToPatient(Patient1, room1);
+            hospital.AssignRoomToPatient(Patient2 , room2);
+
+            Patient1.DisplayInfo();
+            Patient2.DisplayInfo();
+
+            hospital.GetDoctorPatient(doctor1);
+            hospital.GetDoctorPatient(doctor2);
         }
     }
 }
--- Room.cs
+++ Room.cs
@@ -6,15 +6,34 @@ using System.Threading.Tasks;
 
 namespace Hospital_MS
 {
+    public enum RoomType
+    {
+        General,
+        ICU,
+        OperationTheater
+    }
+
     public class Room
     {
         public int RoomNumber {  get; set; }
+        public RoomType RoomType { get; set; }
         public bool IsOccupied { get; set; }
 
 
-        public Room (int roomNumber)
+        public Room (int roomNumber, RoomType roomType)
         {
             RoomNumber = roomNumber;
+            RoomType = roomType;
+            IsOccupied = false;
+        }
+
+        public void OccupyRoom()
+        {
+            IsOccupied = true;
+        }
+
+        public void VacateRoom()
+        {
             IsOccupied = false;
         }
     }
