e5094b2e89f8d4db01894023dd25f6a4a0e60fbf
Rashed Al Ghailani <Authority217@gmail.com>
2024-09-30 09:27:54 +0400

    Finished Appointment Class

--- Appointment.cs
+++ Appointment.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Hospital_MS
 {
-    internal class Appointment
+    public class Appointment
     {
+        public Patient Patient { get; set; }
+
+        public Doctor Doctor { get; set; }
+
+        public DateTime AppointmentDate { get; set; }
+
+        public Appointment(Patient patient, Doctor doctor)
+        {
+            Patient = patient;
+            Doctor = doctor;
+        }
+
+        public void ScheduleAppointment(DateTime date)
+        {
+            AppointmentDate = date;
+        }
+
+        public void CancelAppointment()
+        {
+            AppointmentDate = default(DateTime);
+        }
+
+        public void GetAppointmentDetails()
+        {
+            Console.WriteLine($"Appointment for Patient: {Patient.Name}, Doctor: {Doctor.Name}, Date: {AppointmentDate}");
+        }
     }
 }
