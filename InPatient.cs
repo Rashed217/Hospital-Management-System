48cc173750953c212d198d7292b8b933ea865671
Rashed Al Ghailani <Authority217@gmail.com>
2024-10-01 14:07:15 +0400

    Added classes for (In-Patient) and (Out-Patient)

---
+++ InPatient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_MS
+{
+    public class InPatient : Patient
+    {
+
+        public Room Room { get; set; }
+        public Doctor AssignedDoctor { get; set; }
+        public DateTime AdmissionDate { get; set; }
+
+        public InPatient(int patientId, string name, int age, Gender gender, string ailment, Doctor assignedDoctor) : base(patientId, name, age, gender, ailment, assignedDoctor)
+        {
+            AssignedDoctor = assignedDoctor;
+            AdmissionDate = DateTime.Now;
+        }
+
+        public void AssignRoom(Room room)
+        {
+            Room = room;
+            room.OccupyRoom();
+        }
+
+        public void Discharge()
+        {
+            Room.VacateRoom();
+            Room = null;
+            AssignedDoctor = null;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Room: {Room?.RoomNumber}, Assigned Doctor: {AssignedDoctor?.Name}");
+        }
+    }
+}
---
+++ OutPatient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_MS
+{
+    public class OutPatient : Patient
+    {
+
+        public Clinic ClinicAssigned { get; set; }
+
+        public OutPatient(int patientId, string name, int age, Gender gender, string ailment, Doctor assignedDoctor) : base(patientId, name, age, gender, ailment, assignedDoctor) { }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Clinic Assigned: {ClinicAssigned?.ClinicName}");
+        }
+    }
+}
